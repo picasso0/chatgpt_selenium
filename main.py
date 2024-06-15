c2fd917a9fbed9f646ff1bb0885635deb8073ae5
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-06-16 01:54:28 +0330

    fix a simple main api

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from chatgpt_automatic import ChatGPTAutomator
-breakpoint()
+from fastapi import FastAPI, HTTPException, Header,Query
+from typing import Dict, List
+from pydantic import BaseModel
+import asyncio
+import os
+from time import sleep
+
 chatgpt = ChatGPTAutomator()
-question = "How ChatGPT Ignited the A.I. Competition Wave"
 
-chatgpt.send_prompt_to_chatgpt(question)
 
-# wait a moment ...
 
-answer = chatgpt.return_last_response()
 
-print(answer)
+
+app = FastAPI()
+
+
+@app.post("/chat/")
+async def chat(question:str=Query("")):
+    breakpoint()
+    if question==None or question=="":
+        return "enter a message"
+
+    chatgpt.send_prompt_to_chatgpt(question)
+    answer = chatgpt.return_last_response()
+
+    return {"answer":answer}
+
