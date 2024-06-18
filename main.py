368871ce92dbccce4a597bfa7c70b65eb208ceec
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-06-19 01:43:31 +0330

    fix main usage

--- main.py
+++ main.py
@@ -1,22 +1,96 @@
 from chatgpt_automatic import ChatGPTAutomator
-from fastapi import FastAPI, HTTPException, Header,Query
+from fastapi import FastAPI, HTTPException, Header,Query, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict, List
-from pydantic import BaseModel
-import asyncio
-import os
-from time import sleep
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from auth import (
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+    Token,
+    UserInRequest,
+    oauth2_scheme,
+    HTTPException
+)
+from db import get_database
 
-chatgpt = ChatGPTAutomator()
+
+async def get_db_instance():
+    database = await get_database()
+    return database
+
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+class UserChatGPTSessionManager:
+    def __init__(self):
+        self.chatgpt_sessions = {}
+
+    def get_session(self, user_id: int):
+        if user_id in self.chatgpt_sessions:
+            return self.chatgpt_sessions[user_id]
+        else:
+            return None
+
+    def create_session(self, user_id: int):
+        session = ChatGPTAutomator(user_id)
+        self.chatgpt_sessions[user_id] = session
+        return session
+
+    def delete_session(self, user_id: int):
+        if user_id in self.chatgpt_sessions:
+            self.chatgpt_sessions[user_id].quit()
+            del self.chatgpt_sessions[user_id]
+
+user_chatgpt_session_manager = UserChatGPTSessionManager()
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(input: UserInRequest):
+    user = await authenticate_user(input.username, input.password)
+    if not user:
+        credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+        raise credentials_exception
+        
+        
+    access_token = create_access_token(data={"sub": user["username"]})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/create_chatgpt_session")
+async def create_chatgpt_session(current_user: dict = Depends(get_current_user)):
+    user_id = str(current_user.get('_id'))
+    user_chatgpt_session_manager.create_session(user_id)
+    return {"msg":"chatgpt session created"}
+    
 
 @app.post("/chat/")
-async def chat(question:str=Query("")):
+async def chat(current_user: dict = Depends(get_current_user), question:str=Query()):
     if question==None or question=="":
         return "enter a message"
-
+    user_id = str(current_user.get('_id'))
+    chatgpt = user_chatgpt_session_manager.get_session(user_id)
     chatgpt.send_prompt_to_chatgpt(question)
     answer = chatgpt.return_last_response()
 
     return {"answer":answer}
+
+
+@app.post("/quit/")
+async def quit(current_user: dict = Depends(get_current_user), question:str=Query()):
+    user_id = str(current_user.get('_id'))
+    chatgpt = user_chatgpt_session_manager.get_session(user_id)
+    chatgpt.quit()
+    return {"msg":"chatgpt session deleted"}
 
