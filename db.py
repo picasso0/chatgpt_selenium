d581348f7a87ae77009a2bd4e729b31ce55eefe6
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-06-19 01:43:25 +0330

    fix db info

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import motor.motor_asyncio
 
-
 async def get_database():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://77.238.108.86:27000/chatgpt?retryWrites=true&w=majority")
-    db = client.gathering
+        "mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
+    db = client.chatgpt
     return db
