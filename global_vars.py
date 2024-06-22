7b9dd68d98cbff28b37dbb0da635835c612636a4
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-06-23 01:14:35 +0330

    add global_vars

---
+++ global_vars.py
@@ -0,0 +1,10 @@
+MINIO_PROTOCOL="https"
+MINIO_URL="minio.sirafgroup.com"
+MINIO_ACCESS_KEY="7qMlPjxSnrQ6fKGCPpl3"
+MINIO_SECRET_KEY="zuf+RJCJY2rlmuU3WYI7ztbyvnKqq4N7bIdt1AumjA82"
+MINIO_BUCKET_NAME="chatgpt"
+
+MONGODB_URL="mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority"
+
+AUTH_JWT_SECRET_KEY = "your_secret_key"
+AUTH_JWTALGORITHM = "HS256"
