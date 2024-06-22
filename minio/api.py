1a46cfbb0640cd4947e490629458794c071dcc36
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-06-23 01:13:39 +0330

    add minio app

---
+++ minio/api.py
@@ -0,0 +1,26 @@
+from main import app
+from fastapi.responses import JSONResponse
+from fastapi import Depends, HTTPException, UploadFile, File
+from auth.auth import get_current_bot
+from minio.minio import MinioClass
+from db import get_database
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'zip'
+
+
+@app.post("/upload_userdata/")
+async def upload_zip_file(current_user: dict = Depends(get_current_bot), file: UploadFile = File(...)):
+    
+    if not allowed_file(file.filename):
+        raise HTTPException(status_code=400, detail="Only zip files are allowed.")
+    db = await get_database()
+    minio_obj=MinioClass()
+    try:
+        file_url = await minio_obj.upload_file(file)
+        user_data =  await db.user_data.insert_one({'status': 0,'path': file_url})
+        if not user_data.acknowledged:
+            JSONResponse(content="cannot create userdata", status_code=500)        
+        return {"message": f"File '{file.filename}' uploaded successfully."}
+    except Exception as err:
+        JSONResponse(content={"error": str(err)}, status_code=500)
---
+++ minio/minio.py
@@ -0,0 +1,27 @@
+from minio.minio import Minio
+from io import BytesIO
+from global_vars import MINIO_URL, MINIO_ACCESS_KEY, MINIO_BUCKET_NAME, MINIO_SECRET_KEY, MINIO_PROTOCOL
+
+class MinioClass():
+    def __init__(self) -> None:
+        self.minio_client = Minio(
+        MINIO_URL,
+        access_key=MINIO_ACCESS_KEY,
+        secret_key=MINIO_SECRET_KEY,
+        secure=False
+    )
+        try:
+            self.minio_client.make_bucket(MINIO_BUCKET_NAME)
+        except Exception as err:
+            print(err)
+            
+    async def upload_file(self, file):
+        content = await file.read()
+        self.minio_client.put_object(
+            "chatgpt",
+            file.filename,
+            BytesIO(content),
+            len(content)
+        )
+        return f"{MINIO_PROTOCOL}://{MINIO_URL}/{MINIO_BUCKET_NAME}/{file.filename}"
+        
