9696605a19f71cf233ce937c8496a6c269db687e
Your Name <you@example.com>
2024-06-23 10:33:50 -0400

    add Promt schema

--- chatgpt/schema.py
+++ chatgpt/schema.py
@@ -1,4 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Question(BaseModel):
     question: str
+    
+class Promt(BaseModel):
+    promt: str
+    type: str
+    window_id: Optional[str]
