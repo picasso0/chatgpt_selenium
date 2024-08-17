3ca1e2cd90b360da1db561abb4243b75b9b39264
you@example.com <you@example.com>
2024-08-17 14:54:35 +0330

    fix select bot with windows counts

--- db.py
+++ db.py
@@ -27,7 +27,7 @@ class DatabaseClass:
         
     async def insert_window(self, gpt_type: str, public: int = 0):
         now_datetime = datetime.now()
-        bot = await self.db.bots.find_one(sort=[("windows_number", 1)])
+        bot = await self.db.bots.find_one(sort=[("window_counts", 1)])
         window_data = {'bot_id':ObjectId(bot['_id']), 'create_at':now_datetime, 'status':2, "public_window": public}
         window = await self.db.windows.insert_one(window_data)
         await self.db.bots.update_one({"_id":ObjectId(bot['_id'])},{'$inc': {'window_counts': 1}})
